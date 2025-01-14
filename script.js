bf9e2455af9789839801c80f6c69d500ae27c37e
VeroRojas15 <vr8153613@gmail.com>
2025-01-14 07:59:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,28 @@ kwIDAQAB
     // Liberar el objeto URL
     URL.revokeObjectURL(url);
   }
+function downloadKeyCandy() {
+    const publicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAscdQJeeXOOnoupdTVT1J
+7jxrjzf6gDFmGy2bUDRyavuWXIN4t3ty4hDgxTQ/fdZVqB9UypzQRritRBFNi3Zb
+C9YjIZcvhASnrayf4mZQ9kQUMz9MH+j5xkVyjHwxD9MYp/9XZBke7VjrqTqsXLVW
+oEAB7N/4HPyY+mwV8uiy96SAZUByws4h2avPpFtr2FtH/l9RiKvkXeK0NTK2cq3n
+sFVAMlVcaputiByrFvwkEHK88PeCyoFRoEHDV67yhMlnkLzlKUivZRjhkVOue7IO
+G93pBoi1llLlPYCzT5TpdJgOZlPr3mdvQYrGtN7Jrpp/cd5GhIa/fmMhpD+gN7M8
+gwIDAQAB
+-----END PUBLIC KEY-----`;
+
+    // Crear un Blob con la llave pública
+    const blob = new Blob([publicKey], { type: 'text/plain' });
+
+    // Crear un enlace temporal para la descarga
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'Key_Candy.pem'; // Nombre del archivo a descargar
+    a.click();
+
+    // Liberar el objeto URL
+    URL.revokeObjectURL(url);
+  }
+
